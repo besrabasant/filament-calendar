ba75529eadbcd515a591c28167637700e9dd65df
Basant Besra <besrabasant@gmail.com>
2022-02-10 23:45:25 +0530

    Add Calendar Component

---
+++ src/Http/Livewire/Calendar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Filament\FilamentCalendar\Http\Livewire;
+
+use Livewire\Component;
+
+class Calendar extends Component
+{
+    public function render()
+    {
+        return view("filament-calendar::calendar");
+    }
+}
